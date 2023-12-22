47b59cb9633b1503f6a16f0a4f1bfaac10769b76
minjj0905 <minjj0905@daum.net>
2023-12-22 13:53:14 +0900

    feat: popup 카드 정보 수정

--- front/src/components/Sidebar/Content/PopupCard.vue
+++ front/src/components/Sidebar/Content/PopupCard.vue
@@ -1,21 +1,22 @@
 <template>
   <fwb-card class="cursor-pointer p-3">
-    <div class="flex justify-between gap-3 text-sm">
-      <div class="flex justify-between w-[16rem] max-h-[6rem] flex-col">
+    <div class="flex justify-between gap-2 text-sm">
+      <div class="flex w-[16rem] min-h-[6rem] flex-col">
         <h2
-          class="font-bold text-purple-600 text-lg flex justify-between items-center"
+          class="font-bold text-purple-600 text-lg flex justify-between gap-2 items-start"
         >
           {{ store?.name }}
-          <fwb-badge class="m-0" type="purple">{{ store?.category }}</fwb-badge>
+          <fwb-badge class="m-0 mt-1 flex-shrink-0" type="purple">{{
+            store?.category
+          }}</fwb-badge>
         </h2>
         <div>
           <span class="font-semibold mr-1">
             {{ store?.locale }}
           </span>
           <span>
-            {{ dayjs(store?.startDate).format('M.D') }}~{{
-              dayjs(store?.endDate).format('M.D')
-            }}
+            {{ dayjs(store?.startDate).format('M.D') }} ~
+            {{ dayjs(store?.endDate).format('M.D') }}
           </span>
         </div>
         <p class="text-gray-500">
@@ -23,7 +24,7 @@
         </p>
       </div>
       <img
-        class="w-[6.5rem] h-[5.5rem] object-cover rounded-md bg-gray-100"
+        class="w-[6.5rem] min-h-[5.5rem] max-h-[6.5rem] object-cover rounded-md bg-gray-100"
         :src="imgData?.items[0].link"
         onerror="this.src = 'noimg.png'"
       />
